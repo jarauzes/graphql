128b80a4224e6ec653a4ce3f93a7c0fab3dad685
Joe Ydoan Rauzes <jarauzes@hotmail.com>
2019-04-18 15:16:03 +0200

    S2L001: GraphQL Queries, Nested GraphQL Queries, Babel & ES6 Import/Export

--- basics/src/index.js
+++ basics/src/index.js
@@ -1,1 +1,13 @@
-console.log("hello GraphQL");
+import myCurrentLocation1 from './myModule'; // also acceptable
+import myCurrentLocation2, { getGreeting, message, name } from './myModule';
+import addNumber, {subtract} from './math' 
+
+
+console.log('------------------------------------------------myModule.js');
+console.log(myCurrentLocation1, myCurrentLocation2);
+console.log(message, name);
+console.log(getGreeting('Joe Ydoan Rauzes'));
+console.log('------------------------------------------------math.js');
+console.log('Add (a, b): ', addNumber(1, -2));
+console.log('Subtract (a, b): ', subtract(10, 5));
+console.log('------------------------------------------------END');
