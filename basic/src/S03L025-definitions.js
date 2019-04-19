cde15a8160df7fa845b821d2829f5175e38c1cf8
Joe Ydoan Rauzes <jarauzes@hotmail.com>
2019-04-19 14:36:02 +0200

    S03L025-S03L025: Creating Data with Mutations with Exercise

--- basic/src/S03L025-definitions.js
+++ basic/src/S03L025-definitions.js
@@ -11,7 +11,6 @@ const typeDefs = `
     type Mutation {
         createUser(name: String!, email: String!, age: Int): User!
         createPost(title: String!, body: String!, published: Boolean!, author: ID!): Post!
-        createComment(text: String!, author: ID!, post: ID!): Comment!
     }
 
     type User {
