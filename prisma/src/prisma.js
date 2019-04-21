c8ed52fea07d95d28a9e44378839ccc8a10e913d
Joe Ydoan Rauzes <jarauzes@hotmail.com>
2019-04-21 20:19:47 +0200

    S06L066-S06L066: Prisma Authorization forcing client to go through node.js

--- prisma/src/prisma.js
+++ prisma/src/prisma.js
@@ -2,7 +2,8 @@ import { Prisma } from 'prisma-binding'
 
 const prisma = new Prisma({
     typeDefs: 'prisma/src/gen/prisma.graphql',
-    endpoint: 'http://localhost:4466'
+    endpoint: 'http://localhost:4466',
+    secret: 'mypass22mypassMP'
 })
 
 export { prisma as default }
