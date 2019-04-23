16440426bf5731392fde64d659a2661ef569245a
Joe Ydoan Rauzes <jarauzes@hotmail.com>
2019-04-23 20:47:41 +0200

    84% | S09L094-S09L098: Setting up Test Environment, https://parceljs.org/

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,7 +1,26 @@
-test('This is my first test case', () => {
-    1 + 2
+import { getFirstName, isValidPassword } from '../prisma/src/utils/user'
+
+test('Should return name when given fullName', () => {
+    const firstName = getFirstName('Joey Suarez')
+    expect(firstName).toBe('Joey')
 })
 
-test('This is my second test case', () => {
-    
+test('Should return name when given only frist name', () => {
+    const firstName = getFirstName('dale')
+    expect(firstName).toBe('dale')
+})
+
+test('Should reject password Shorter than 8 Characters', () => {
+    const isValid = isValidPassword('dale')
+    expect(isValid).toBe(false)
+})
+
+test('Should reject password containing word "password"', () => {
+    const isValid = isValidPassword('dalepassWord12345')
+    expect(isValid).toBe(false)
+})
+
+test('Should reject password Valid', () => {
+    const isValid = isValidPassword('dale12345')
+    expect(isValid).toBe(true)
 })
