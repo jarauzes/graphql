b346e98091685b660ac77d73c52c0f2a5b58168b
Joel Suarez <admin@webtonator.com>
2019-04-23 15:25:49 +0200

    80% | S08L089-S08L093: Production Deployment & Environment Variables

--- prisma/src/prisma.js
+++ prisma/src/prisma.js
@@ -4,7 +4,7 @@ import { fragmentReplacements } from './resolvers/index'
 const prisma = new Prisma({
     typeDefs: 'prisma/src/gen/prisma.graphql',
     endpoint: process.env.PRISMA_ENDPOINT,
-    secret: 'mypass22mypassMP',
+    secret: process.env.PRISMA_SECRET,
     fragmentReplacements
 })
 
